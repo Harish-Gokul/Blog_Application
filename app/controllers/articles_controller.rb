f85a1b6723fcc5e19026dcbcbd981162b862c2f3
Harish Gokul G <mtpharishgokul@gmail.com>
2023-08-29 07:21:01 +0530

    added feature - create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+  def create
+    @article = Article.new
+    @article.title = params[:title]
+    @article.description = params[:description]
+    @article.save
+    redirect_to "/articles/#{@article.id}"
+  end
+
 end
