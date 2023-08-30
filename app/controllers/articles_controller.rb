d0b439afc517bd401b3d7e073932bf7bb2485c0c
Harish Gokul G <mtpharishgokul@gmail.com>
2023-08-30 06:30:45 +0530

    Added functionality - display all records in index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,15 +7,36 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new
-
-  end
-  def create
+    def new
     @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
-    redirect_to "/articles/#{@article.id}"
+    end
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to "/articles/#{@article.id}"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article = Article.update(params.require(:article).permit(:title,:description))
+    if @article.save
+      flash[:notice]  = "Article has been edited successfully"
+      redirect_to article_path
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+
   end
 
 end
