efb16f71a9e16b5215115a90e05a15055ccd72ce
Harish Gokul G <mtpharishgokul@gmail.com>
2023-09-10 06:49:11 +0530

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :articles
+  VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
+  validates :username, presence: true,uniqueness: true,length: {minimum: 5,maximum: 25}
+  validates :email,presence: true,uniqueness: true,length: {maximum:250},format: { with: VALID_EMAIL_REGEX ,multiline:true}
+
 end
