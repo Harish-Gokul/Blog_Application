f5d39e4d7338bc82e0f05c7377af78b85945b51e
Harish Gokul G <mtpharishgokul@gmail.com>
2023-09-12 11:05:13 +0530

    implemented user login and logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  def  create
+    @user = User.find_by(params.require(:session).permit(:email))
+
+    if @user && @user.authenticate(params.require(:session).permit(:password)[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "Your credential was not right"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "successfully logged out"
+    redirect_to root_path
+  end
+
+end
